7360e101f716c11374bfb6fdd2674050e337bc57
YgriKs <92621000+YgriKs@users.noreply.github.com>
2022-03-01 18:49:42 +0300

    Add files via upload

---
+++ Money/Money/Money.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Money
+{
+    class Money
+    {
+        private int denomination;
+        private int count;
+        private int N;
+
+        public Money(int denomination, int count, int N) 
+        {
+            this.denomination = denomination;
+            this.count = count;         
+            this.N = N;
+        }
+
+        public void Show() 
+        {
+            Console.WriteLine("\nНоминал купюры = {0}\nКоличество Купюр = {1}", denomination, count);
+        }
+
+        public bool Enough() 
+        {
+            if ((denomination * count) > N)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int Howmany() 
+        {
+            return (denomination * count) / N;      
+        }
+
+        public int Denomination
+        {
+            get { return denomination; }
+            set { denomination = value; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+    }
+}
---
+++ Money/Money/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m1 = new Money(100, 5, 85);
+            m1.Show();
+            Console.WriteLine(m1.Enough());
+            Console.WriteLine(m1.Howmany());
+        }
+    }
+}
